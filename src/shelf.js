9c755e0e8cdc22840450e32db365838c5a87f5c5
Anna Spitz <aspitzemail@gmail.com>
2022-05-14 12:53:43 -0500

    Add shelf and shelf-test wip

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,34 @@
-
+// try an object
+function shelfBook(book, genreShelf) {
+  if(book.genre === 'sciFi' && genreShelf.length < 3) {
+    genreShelf.unshift(book)
+    return genreShelf
+  }
+}
+function unshelfBook(bookTitle, genreShelf) {
+  for(i = 0; i < genreShelf.length; i++) {
+    if(genreShelf[i].title === bookTitle) {
+      genreShelf.splice(i, 1)
+      return genreShelf
+    }
+  }
+}
+function listTitles(shelfContent) {
+  var bookTitles = []
+  for(i = 0; i < shelfContent.length; i++) {
+    bookTitles.push(shelfContent[i].title)
+  }
+  bookTitles = bookTitles.join(', ')
+  return bookTitles
+}
+function searchShelf(genreShelf, bookTitle) {
+  Boolen (genreShelf.filter(function(a){
+    return a.title === bookTitle
+  }))
+}
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
