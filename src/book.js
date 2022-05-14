7517229691812c7780400b247504f1259b3d1952
Anna Spitz <aspitzemail@gmail.com>
2022-05-14 12:51:53 -0500

    Add book and book-test work

--- src/book.js
+++ src/book.js
@@ -1,12 +1,52 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return 'The ' + title;
 }
 
+function buildMainCharacter(charName, charAge, charPronouns) {
+  var charSheet = {
+    name: charName,
+    age: charAge,
+    pronouns: charPronouns,
+   }
+  return charSheet
+}
+
+function saveReview(uniqueReviews, readerReviews) {
+  if(readerReviews.includes(uniqueReviews)) {
+    return readerReviews
+  }
+  readerReviews.push(uniqueReviews)
+  return readerReviews
+}
+
+function calculatePageCount(bookTitle) {
+  var pageCount = bookTitle.length * 20
+  return pageCount
+  // var pagesInBook = 0
+  // for (var i =0; i < title.length; i++) {
+  //   pagesInBook = pagesInBook + 20
+  // }
+  // return pagesInBook
+}
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: bookGenre,
+  }
+  return book
+}
+function editBook(bookToEdit) {
+  bookToEdit.pageCount = ((bookToEdit.pageCount) * .75)
+  return bookToEdit.pageCount
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
